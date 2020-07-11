4d964876e9aca4bf4b4682ac4f98bd692df3369b
yarov-475 <ya.yarochkin@yandex.ru>
2020-07-11 19:02:09 +0300

    buld

--- firstsite/src/Pages/About.js
+++ firstsite/src/Pages/About.js
@@ -1,9 +1,44 @@
 import React, {Component} from "react";
+import one from  './../assets/backStage.jpg';
+import two from './../assets/bst3.jpg' ;
+import Tab from "react-bootstrap/Tab";
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
+
+
 export default class About extends Component{
     render() {
         return(
-            <div>
-                <h2>About</h2>
+            <div className="tabAll">
+                <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                    <Row>
+                        <Col sm={3}>
+                            <Nav variant="pills" className="flex-column">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Технические службы</Nav.Link>
+
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Творческий коллектив</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                        <Col sm={9}>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <img src={one} alt="" className ="tabImg" />
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi atque beatae commodi debitis deleniti dolore doloremque ea eaque earum eius, fuga, fugiat harum illo laboriosam modi mollitia neque nesciunt nobis nulla, odio officiis optio pariatur perferendis qui quod recusandae sit soluta sunt temporibus vero vitae voluptas voluptate? Accusamus accusantium aliquam animi aperiam at culpa cum cumque dicta ea eligendi esse ex fugit ipsa laboriosam magni nemo neque, quam quas quo repudiandae velit, voluptate voluptates? Doloribus error ex, exercitationem . Aliquam.</p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                   <img src={two} alt=""className ="tabImg"/ >
+                                   <p>Lorem ipsum dolor sit ametLorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi atque beatae commodi debitis deleniti dolore doloremque ea eaque earum eius, fuga, fugiat harum illo laboriosam modi mollitia neque nesciunt nobis nulla, odio officiis optio pariatur perferendis qui quod recusandae sit soluta sunt temporibus vero vitae voluptas voluptate? Accusamus accusantium aliquam animi aperiam at culpa cum cumque dicta ea eligendi esse ex fugit ipsa laboriosam magni nemo neque, quam quas quo repudiandae velit, voluptate voluptates? Doloribus error ex, exercitationem . Aliquam. </p>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Col>
+                    </Row>
+                </Tab.Container>
             </div>
 
         )
--- firstsite/src/Pages/Contacts.js
+++ firstsite/src/Pages/Contacts.js
@@ -1,8 +1,56 @@
 import React, {Component} from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 export default class Contacts extends Component{
     render() {
         return(
-            <h1>Contacts</h1>
+            <div>
+                <h1>подпишись на рассылку</h1>
+                <Form>
+                    <Form.Row className="align-items-center">
+                        <Col xs="auto">
+                            <Form.Label htmlFor="inlineFormInput" srOnly>
+                                Name
+                            </Form.Label>
+                            <Form.Control
+                                className="mb-2"
+                                id="inlineFormInput"
+                                placeholder="Jane Doe"
+                            />
+                        </Col>
+                        <Col xs="auto">
+                            <Form.Label htmlFor="inlineFormInputGroup" srOnly>
+                                Username
+                            </Form.Label>
+                            <InputGroup className="mb-2">
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text>@</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                {/* eslint-disable-next-line react/jsx-no-undef */}
+                                <FormControl id="inlineFormInputGroup" placeholder="Username" />
+                            </InputGroup>
+                        </Col>
+                        <Col xs="auto">
+                            <Form.Check
+                                type="checkbox"
+                                id="autoSizingCheck"
+                                className="mb-2"
+                                label="Remember me"
+                            />
+                        </Col>
+                        <Col xs="auto">
+                            <Button type="submit" className="mb-2">
+                                Submit
+                            </Button>
+                        </Col>
+                    </Form.Row>
+                </Form>
+
+            </div>
+
         )
     }
 
